8bae292f90d31376bf3c655d75315de00d82fb8d
Julian Guterman <gutermanj@gmail.com>
2016-03-12 18:55:41 -0500

    Fixed email thing

--- db/migrate/20160312234556_add_info_to_contact.rb
+++ db/migrate/20160312234556_add_info_to_contact.rb
@@ -2,7 +2,5 @@ class AddInfoToContact < ActiveRecord::Migration
   def change
     add_column :contacts, :name, :string
     add_column :contacts, :email, :string
-    add_column :contacts, :phonenumber, :string
-    add_column :contacts, :message, :text
   end
 end
